0af49399012bab954d0d33d9cb57b33f54320c92
maxsusladev <maxsusladev@gmail.com>
2026-03-12 13:36:33 +0100

    features: added-exepsion-handling-and-transactionsTypeOrm

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, RequestTimeoutException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersService } from 'src/users/providers/users.service';
 import { Post } from '../post.entity';
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 import { CreatePostDto } from '../dtos/create-post.dto';
 import { MetaOption } from 'src/meta-options/meta-option.entity';
 import { TagsService } from 'src/tags/providers/tags.service';
+import { PatchPostDto } from '../dtos/patch-posts.dto';
 
 
 @Injectable()
@@ -20,7 +21,6 @@ export class PostsService {
         private readonly tagsService: TagsService
     ) { }
 
-
     public async createPost(createPostDto: CreatePostDto) {
 
         let author = await this.userService.findById(createPostDto.authorId)
@@ -28,31 +28,30 @@ export class PostsService {
         let tags;
 
 
-        if(createPostDto.tags) {
+        if (createPostDto.tags) {
             tags = await this.tagsService.findultipleTAgs(createPostDto.tags)
         }
-        if(author) {
+        if (author) {
             post = this.postsRepositiry.create({
                 ...createPostDto,
                 author,
                 tags,
             })
         }
-        
-       
+
+
         return await this.postsRepositiry.save(post)
     }
-
 
     public async findAll(userId: number) {
         const user = this.userService.findById(userId)
         console.log('ddd')
         let posts = await this.postsRepositiry.find({
-           relations: {
-            metaOptions: true,
-            author: true,
-            tags: true
-           }
+            relations: {
+                metaOptions: true,
+                author: true,
+                tags: true
+            }
 
         })
         return posts
@@ -60,12 +59,56 @@ export class PostsService {
     }
 
     public async deletePost(id: number) {
-       
+
         await this.postsRepositiry.delete(id)
 
-       return {
-        id,
-        deleted: true
-       }
+        return {
+            id,
+            deleted: true
+        }
+    }
+
+    public async update(patchPostDto: PatchPostDto) {
+        let tags;
+        let post;
+
+        try {
+            tags = await this.tagsService.findultipleTAgs(patchPostDto.tags ?? [])
+        } catch (error) {
+            throw new RequestTimeoutException("Unable to process your request at the moment please try again later")
+        }
+
+        if (!tags || tags.lengs !== patchPostDto.tags?.length) {
+            throw new BadRequestException("Please check your tags id's and ensure they are correct")
+        }
+
+        try {
+            post = await this.postsRepositiry.findOneBy({ id: patchPostDto.id })
+        } catch (error) {
+            throw new RequestTimeoutException("Unable to process your request at the moment please try again later")
+        }
+
+        if (!post) {
+            throw new BadRequestException(`Post with id ${patchPostDto.id} not found`)
+        }
+
+        Object.assign(post, {
+            title: patchPostDto.title ?? post.title,
+            content: patchPostDto.content ?? post.content,
+            status: patchPostDto.status ?? post.status,
+            postType: patchPostDto.postType ?? post.postType,
+            slug: patchPostDto.slug ?? post.slug,
+            schema: patchPostDto.schema ?? post.schema,
+            featuredImageUrl: patchPostDto.featuredImageUrl ?? post.featuredImageUrl,
+            publishOn: patchPostDto.publishOn ?? post.publishOn,
+        })
+
+
+        try {
+            await this.postsRepositiry.save(post)
+        } catch (error) {
+            throw new RequestTimeoutException("Unable to process your request at the moment please try again later")
+        }
+        return post
     }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,63 +1,64 @@
-import { Controller, Get, Param, Query, Post, Body, Req, ParseIntPipe, ValidationPipe, Patch} from '@nestjs/common';
+import { Controller, Get, Param, Query, Post, Body, Req, ParseIntPipe, ValidationPipe, Patch } from '@nestjs/common';
 import { Request } from 'express';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUSersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user-dto';
 import { UsersService } from './providers/users.service';
 import { ApiTags, ApiQuery, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { CreateManyUsersDto } from './dtos/create-many-user.dto';
 
 
 @Controller('users')
 @ApiTags('users')
 export class UsersController {
-        constructor(
-            private readonly userService: UsersService
-        ){
+    constructor(
+        private readonly userService: UsersService
+    ) {
 
-        }
-
-@Get('{/:id}')
-@ApiOperation({
-    summary: "fetches a list of registred users on the application",
-})
-@ApiResponse({
-    status:200,
-    description: 'users fetched successfully based on the query'
-})
-@ApiQuery({
-    name: 'limit',
-    type: 'number',
-    required: false,
-    description: 'the number of entries returned per query',
-    example: 10,
-})
-@ApiQuery({
-    name: 'page',
-    type: 'number',
-    required: false,
-    description: 'the position of the number that you wants',
-    example: 2,
-})
-    public getUsers(
-        @Param() getUsersPaaramDto: GetUSersParamDto,
-        @Query('limit', ParseIntPipe) limit:number ,
-        @Query('page', ParseIntPipe) page:number ,
-    ){
-
-        return this.userService.findAll(getUsersPaaramDto, limit,page)
     }
 
+    @Get('{/:id}')
+    @ApiOperation({
+        summary: "fetches a list of registred users on the application",
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'users fetched successfully based on the query'
+    })
+    @ApiQuery({
+        name: 'limit',
+        type: 'number',
+        required: false,
+        description: 'the number of entries returned per query',
+        example: 10,
+    })
+    @ApiQuery({
+        name: 'page',
+        type: 'number',
+        required: false,
+        description: 'the position of the number that you wants',
+        example: 2,
+    })
+    public getUsers(
+        @Param() getUsersPaaramDto: GetUSersParamDto,
+        @Query('limit', ParseIntPipe) limit: number,
+        @Query('page', ParseIntPipe) page: number,
+    ) {
 
-@Post()
-    public createUsers(@Body() createUserDto: CreateUserDto ){
-       
+        return this.userService.findAll(getUsersPaaramDto, limit, page)
+    }
+    @Post()
+    public createUsers(@Body() createUserDto: CreateUserDto) {
         return this.userService.createUser(createUserDto);
     }
 
+    @Post('create-many')
+    public createManyUsers(@Body() createManyUsersDto: CreateManyUsersDto) {
+        return this.userService.createMany(createManyUsersDto);
+    }
+
     @Patch()
-    public patchUser(@Body() patchUserDto: PatchUserDto){
-
-
+    public patchUser(@Body() patchUserDto: PatchUserDto) {
         return patchUserDto;
     }
 }
